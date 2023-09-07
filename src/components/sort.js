fc935c895b465abb74cd46f039f14e944848f486
Yurkov <buzia3lic@gmail.com>
2023-09-07 18:17:34 +0300

    still sort

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,55 +1,66 @@
 var a = {
-    history: [
-      { data: "2019-01-01" },
-      { data: "04 01 2023" },
-      { data: "07/01/2023" },
-      { data: "04.01.2022" },
-      { data: "04.05.2022" },
-      { data: "04.01.2021" }
-    ]
-  };
-  
-  //a.history.map((history) => {
-  //console.log(new Date(history.data).toLocaleDateString())
-  //})
-  
-  const arr = [];
-  a.history.map((history) => {
-    arr.push(new Date(history.data));
-  });
-  
-  console.log(arr);
-  
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-  console.info(arr);
-  
-  //arr.map((el) => {
-  //console.info(el.getFullYear())})
-  
-  const arr2 = [];
-  var j = 0;
-  var k = 0;
-  arr2[j] = [];
-  arr2[0][0] = arr[0];
-  //console.log(arr2[0][0].getFullYear());
-  
-  for (var i = 1; i < arr.length; i++) {
-    if (arr2[j][k] === arr[i]) {
-      continue;
+  fio: "Yurkov",
+  history: [
+    { data: "2019-01-01" },
+    { data: "04 01 2023" },
+    { data: "07/01/2023" },
+    { data: "04.01.2022" },
+    { data: "04.05.2022" },
+    { data: "04.01.2021" },
+    { data: "04.01.2020" }
+  ]
+};
+
+const arr = [];
+a.history.map((history) => {
+  arr.push(new Date(history.data));
+});
+
+console.log(arr);
+
+arr.sort(function (a, b) {
+  return a - b;
+});
+console.info(arr);
+
+const arr2 = [];
+var j = 0;
+var k = 0;
+arr2[j] = [];
+arr2[0][0] = arr[0];
+
+for (var i = 1; i < arr.length; i++) {
+  if (arr2[j][k] === arr[i]) {
+    continue;
+  } else {
+    if (arr2[j][k].getFullYear() === arr[i].getFullYear()) {
+      arr2[j].push(arr[i]);
+      k++;
     } else {
-      if (arr2[j][k].getFullYear() === arr[i].getFullYear()) {
-        arr2[j].push(arr[i]);
-        k++;
-      } else {
-        j++;
-        arr2[j] = [];
-        arr2[j].push(arr[i]);
-        k = 0;
-      }
+      j++;
+      arr2[j] = [];
+      arr2[j].push(arr[i]);
+      k = 0;
     }
   }
+}
+
+console.error(arr2);
+
+const groupBy = (ar, key) => {
   
-  console.error(arr2);
-  
+  return ar.reduce(function (rv, x) {
+    (rv[x.key] = rv[x.key] || []).push(x);
+    return rv;
+  }, {});
+};
+
+//const c = a.history.map((history) => new Date(history.data))
+//console.log(c)
+
+//const b = a.history.filter(user => user.data.includes('2019'));
+
+//console.log(b);
+
+
+//console.log(groupBy(c, );
